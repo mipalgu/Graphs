2152ad562a0943d64aa2133387c9e0f980d03624
Callum McColl <callum.mccoll1@gmail.com>
2024-01-13 05:24:51 +1000

    Add docC package plugin.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
             targets: ["Graphs"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
